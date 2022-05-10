8693b6ac215d76ac5d53572ec7ae294e8b2b0a31
MuhktarK <200103521@stu.sdu.edu.kz>
2022-05-10 14:41:08 +0600

    Initial commit

---
+++ realtimechat/routing.py
@@ -0,0 +1,26 @@
+import os
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.urls import path, re_path
+from django.core.asgi import get_asgi_application
+
+from chat.consumers import ChatConsumer
+from public_chat.consumers import PublicChatConsumer
+from notification.consumers import NotificationConsumer
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'realtimechat.settings')
+
+
+application = ProtocolTypeRouter({
+    'http':  get_asgi_application(),
+	
+	'websocket': AuthMiddlewareStack(
+			URLRouter([
+					path('', NotificationConsumer),
+					path('chat/<room_id>/', ChatConsumer),
+					path('public_chat/<room_id>/', PublicChatConsumer),
+			])
+		
+	),
+})
